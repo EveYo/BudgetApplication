8f75c8e98f6928d5f731b47813140b30d9d06159
Eve <126996217+EveYo@users.noreply.github.com>
2024-04-21 07:03:00 -0700

    Done implementing all functions

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ const expenseErrorMessage = document.getElementById("expenseError");
 const budget = document.getElementById("budget");
 const expense = document.getElementById("expense");
 const balance = document.getElementById("balance");
-const list = document.getElementById("listItem");
+const list = document.getElementById("list");
 
 //Variable
 let myBudget = 0;
@@ -24,8 +24,7 @@ let myTotalExpense = 0;
 let myBalance = 0;
 
 setBudgetButton.addEventListener("click", ()=>{
-    myBudget = totalBudget.value; 
-    console.log(myBudget);
+    myBudget = totalBudget.value;
     if (myBudget === null || myBudget < 0){
         budgetErrorMessage.classList.remove("hide");
         // element.classList will return a list of class names of the element
@@ -33,9 +32,82 @@ setBudgetButton.addEventListener("click", ()=>{
     }else{
         budgetErrorMessage.classList.add("hide"); //hide error message
         budget.innerHTML = myBudget; // set the text in the budget html element to whatever
-        balance.innerHTML = myBudget - expense.innerText;  //Update balance
-
+        myBalance = myBudget - myTotalExpense;
+        balance.innerHTML = myBalance;  //Update balance
+        
         //clear input
         totalBudget.value = "";
     }
 })
+
+setExpenseButton.addEventListener("click", ()=>{
+    if (newTitle.value === null || newCost.value === null || newCost.value < 0){
+        expenseErrorMessage.classList.remove("hide");
+        // element.classList will return a list of class names of the element
+        // and by using add or remove, we can add class on the element
+    }else{
+        disableButtons(false); //allow editing
+
+        expenseErrorMessage.classList.add("hide"); //hide error message
+        myTotalExpense += parseInt(newCost.value);
+        expense.innerHTML = myTotalExpense;
+        myBalance = myBudget - myTotalExpense;
+        balance.innerHTML = myBalance;  //Update balance
+
+        //create new item that will be displayed on List
+        createListItem(newTitle.value, newCost.value);
+
+        //clear input
+        newTitle.value = "";
+        newCost.value = "";
+    }
+})
+
+const createListItem = (title, cost) => {
+    let item = document.createElement("div");
+    item.classList.add("list-item", "flex-space");
+    item.innerHTML = `<p class="product">${title}</p><p class="cost">${cost}</p>`;
+
+    let editButton = document.createElement("button");
+    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
+    editButton.style.fontSize = "1.2em";
+    editButton.addEventListener("click", () => {
+        modifyElement(editButton, true);
+    });
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
+    deleteButton.style.fontSize = "1.2em";
+    deleteButton.addEventListener("click", () => {
+        modifyElement(deleteButton);
+    });
+    item.appendChild(editButton);
+    item.appendChild(deleteButton);
+    list.appendChild(item)
+}
+
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach( (element) => {
+        element.disabled = bool;
+    })
+}
+
+const modifyElement = (element, edit= false) => {
+    let parentDiv = element.parentElement;
+    let parentAmount = parentDiv.querySelector(".cost").innerText;
+    
+    if (edit){
+        let parentText = parentDiv.querySelector(".product").innerText;
+        newTitle.value = parentText;
+        newCost.value = parentAmount;
+        disableButtons(true); //while editing, it's not allow to click edit/ delete
+    }
+
+    // Roll Back before this expense
+    myBalance += parseInt(parentAmount);
+    myTotalExpense -= parseInt(parentAmount);
+    balance.innerText = myBalance;
+    expense.innerText = myTotalExpense;
+    
+    parentDiv.remove();
+}
